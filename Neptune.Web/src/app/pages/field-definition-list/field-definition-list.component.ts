66e558b7d60532e36a5521c54be27f0397357363
raynardlee <raynardlee@yahoo.com>
2026-05-08 13:45:11 -0700

    NPT-999: Drop imperative subscribe + cdr.detectChanges from Field Definition list
    Switches the list to the observable + async pipe pattern so zoneless
    change detection fires when the response arrives. Previously the
    component imperatively subscribed and called cdr.detectChanges, which
    in zoneless production left the loading state stuck until the user
    clicked the page.

--- Neptune.Web/src/app/pages/field-definition-list/field-definition-list.component.ts
+++ Neptune.Web/src/app/pages/field-definition-list/field-definition-list.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit, ChangeDetectorRef, ViewChild } from "@angular/core";
-import { AuthenticationService } from "src/app/services/authentication.service";
+import { Component, OnInit } from "@angular/core";
 import { LinkRendererComponent } from "src/app/shared/components/ag-grid/link-renderer/link-renderer.component";
 import { ColDef } from "ag-grid-community";
-import { AgGridAngular, AgGridModule } from "ag-grid-angular";
-import { FieldDefinitionDto, PersonDto } from "src/app/shared/generated/model/models";
+import { AsyncPipe } from "@angular/common";
+import { Observable } from "rxjs";
+import { FieldDefinitionDto } from "src/app/shared/generated/model/models";
 import { FieldDefinitionService } from "src/app/shared/generated/api/field-definition.service";
 import { NeptunePageTypeEnum } from "src/app/shared/generated/enum/neptune-page-type-enum";
 import { AlertDisplayComponent } from "src/app/shared/components/alert-display/alert-display.component";
@@ -14,76 +14,50 @@ import { PageHeaderComponent } from "src/app/shared/components/page-header/page-
     selector: "field-definition-list",
     templateUrl: "./field-definition-list.component.html",
     styleUrls: ["./field-definition-list.component.scss"],
-    imports: [AlertDisplayComponent, AgGridModule, NeptuneGridComponent, PageHeaderComponent],
+    imports: [AlertDisplayComponent, AsyncPipe, NeptuneGridComponent, PageHeaderComponent],
 })
 export class FieldDefinitionListComponent implements OnInit {
-    @ViewChild("fieldDefinitionsGrid") fieldDefinitionsGrid: AgGridAngular;
-
-    private currentUser: PersonDto;
-
-    public fieldDefinitions: Array<FieldDefinitionDto>;
     public richTextTypeID: number = NeptunePageTypeEnum.HippocampLabelsAndDefinitionsList;
 
-    public rowData = [];
+    public fieldDefinitions$: Observable<FieldDefinitionDto[]>;
     public columnDefs: ColDef[];
 
-    constructor(private fieldDefinitionService: FieldDefinitionService, private authenticationService: AuthenticationService, private cdr: ChangeDetectorRef) {}
+    constructor(private fieldDefinitionService: FieldDefinitionService) {}
 
     ngOnInit() {
-        this.authenticationService.getCurrentUser().subscribe((currentUser) => {
-            this.currentUser = currentUser;
-            this.fieldDefinitionsGrid?.api.showLoadingOverlay();
-            this.fieldDefinitionService.listFieldDefinition().subscribe((fieldDefinitions) => {
-                this.fieldDefinitions = fieldDefinitions;
-                this.rowData = fieldDefinitions;
-                this.fieldDefinitionsGrid.api.hideOverlay();
-                this.cdr.detectChanges();
-            });
-            this.columnDefs = [
-                {
-                    headerName: "Label",
-                    valueGetter: function (params: any) {
-                        return { LinkValue: params.data.FieldDefinitionType.FieldDefinitionTypeID, LinkDisplay: params.data.FieldDefinitionType.FieldDefinitionTypeDisplayName };
-                    },
-                    cellRenderer: LinkRendererComponent,
-                    cellRendererParams: { inRouterLink: "/labels-and-definitions/" },
-                    filterValueGetter: function (params: any) {
-                        return params.data.FieldDefinitionType.FieldDefinitionTypeDisplayName;
-                    },
-                    comparator: function (id1: any, id2: any) {
-                        let link1 = id1.LinkDisplay;
-                        let link2 = id2.LinkDisplay;
-                        if (link1 < link2) {
-                            return -1;
-                        }
-                        if (link1 > link2) {
-                            return 1;
-                        }
-                        return 0;
-                    },
-                    sortable: true,
-                    filter: true,
-                    width: 200,
-                },
-                {
-                    headerName: "Definition",
-                    field: "FieldDefinitionValue",
-                    valueGetter: (params: any) => htmlToExcerpt(params.data.FieldDefinitionValue),
-                    sortable: true,
-                    filter: true,
-                    width: 900,
-                    tooltipValueGetter: (params: any) => htmlToExcerpt(params.data.FieldDefinitionValue),
-                },
-            ];
+        this.fieldDefinitions$ = this.fieldDefinitionService.listFieldDefinition();
 
-            this.columnDefs.forEach((x) => {
-                x.resizable = true;
-            });
-        });
-    }
-
-    ngOnDestroy(): void {
-        this.cdr.detach();
+        this.columnDefs = [
+            {
+                headerName: "Label",
+                valueGetter: (params: any) => ({
+                    LinkValue: params.data.FieldDefinitionType.FieldDefinitionTypeID,
+                    LinkDisplay: params.data.FieldDefinitionType.FieldDefinitionTypeDisplayName,
+                }),
+                cellRenderer: LinkRendererComponent,
+                cellRendererParams: { inRouterLink: "/labels-and-definitions/" },
+                filterValueGetter: (params: any) => params.data.FieldDefinitionType.FieldDefinitionTypeDisplayName,
+                comparator: (id1: any, id2: any) => {
+                    if (id1.LinkDisplay < id2.LinkDisplay) return -1;
+                    if (id1.LinkDisplay > id2.LinkDisplay) return 1;
+                    return 0;
+                },
+                sortable: true,
+                filter: true,
+                resizable: true,
+                width: 200,
+            },
+            {
+                headerName: "Definition",
+                field: "FieldDefinitionValue",
+                valueGetter: (params: any) => htmlToExcerpt(params.data.FieldDefinitionValue),
+                sortable: true,
+                filter: true,
+                resizable: true,
+                width: 900,
+                tooltipValueGetter: (params: any) => htmlToExcerpt(params.data.FieldDefinitionValue),
+            },
+        ];
     }
 }
 
